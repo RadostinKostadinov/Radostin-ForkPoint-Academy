4514a63614dc183fda42267e2685d1a819fc8590
Radostin Kostadinov <bg.radostin.kostadinov@gmail.com>
2021-06-01 14:55:12 +0300

    convert curr with only 1 request to infovalutar.ro

--- OnlineStoreTask/routes/common/convertCurrency.js
+++ OnlineStoreTask/routes/common/convertCurrency.js
@@ -2,33 +2,43 @@ const soap = require('soap');
 
 const url = 'http://infovalutar.ro/curs.asmx?wsdl';
 
-function getCurrencyInfo(currency) {
+function getAll() {
   return new Promise((resolve) => {
     soap.createClient(url, (err, client) => {
       if (err) throw err.message;
 
-      client.GetLatestValue({
-        Moneda: currency,
+      const today = new Date();
+      const yesterday = new Date(today);
+
+      yesterday.setDate(yesterday.getDate() - 1);
+
+      const infovalutarReqDate = `${yesterday.toISOString().slice(0, 10)}T00:00:00`;
+
+      client.getall({
+        dt: infovalutarReqDate,
       }, (error, res) => {
         if (error) throw error;
 
-        resolve(res.GetLatestValueResult);
+        resolve(res.getallResult);
       });
     });
   });
 }
 
 module.exports = async function convertCurrency(from, to, value) {
-  const firstCurrency = await getCurrencyInfo(from);
-  const secondCurrency = await getCurrencyInfo(to);
+  const getall = await getAll();
+  const currentCurrencyExRate = Number(getall.diffgram.DocumentElement.Currency
+    .find(currency => currency.IDMoneda === from).Value);
+  const nextCurrencyExRate = Number(getall.diffgram.DocumentElement.Currency
+    .find(currency => currency.IDMoneda === to).Value);
 
-  const result = (value * firstCurrency) / secondCurrency;
+  const result = (value * currentCurrencyExRate) / nextCurrencyExRate;
   return result;
 };
 
 /* async function test() {
-  const newValue = await convertCurrency('USD', 'BGN', 200);
-  console.log(newValue);
+    const result = await convertCurrency('USD', 'BGN', 200);
+    console.log(result);
 }
 
 test(); */
